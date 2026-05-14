9513445e9b7401197a673f6a6d11889fb778fdd2
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2026-05-14 23:44:36 +0100

    feat:Aprimore o tratamento de respostas com mensagens opcionais em respostas de sucesso e erro.

--- backend/common/renderers.py
+++ backend/common/renderers.py
@@ -3,6 +3,9 @@ from rest_framework.renderers import JSONRenderer
 
 class StandardizedJSONRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
+        if isinstance(data, dict) and "success" in data:
+            return super().render(data, accepted_media_type, renderer_context)
+
         if renderer_context and renderer_context.get("response") is not None:
             response = renderer_context["response"]
             if response.status_code >= 400:
