2c509215f38686e97551233b408200653a1ecfea
Nd Daniel <108186371+NdondaDaniel2020@users.noreply.github.com>
2026-05-15 07:55:47 +0100

    feat: Remova o aplicativo Projetos e todos os arquivos relacionados.

--- backend/apps/projects/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
-
-
-class IsOwnerOrAdmin(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        return obj.owner_id == request.user.id or request.user.has_role("admin")
