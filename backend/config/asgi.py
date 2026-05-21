0d96651d8c927482bc6f1a3fa7f28b5bc3b1b716
asebastiao <antoniosebasacs@gmail.com>
2026-05-21 08:30:35 +0100

    feat: add routing websocket

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,7 +1,16 @@
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+from apps.auctions.websocket.middleware import JWTAuthMiddleware
+from apps.auctions.websocket.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": JWTAuthMiddleware(URLRouter(websocket_urlpatterns)),
+    }
+)
