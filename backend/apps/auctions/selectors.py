c425d382c5afc683b6790a1582705122bd936107
asebastiao <antoniosebasacs@gmail.com>
2026-05-21 15:50:05 +0100

    feat: remove unused models, serializers, selectors, and services for better organization

--- backend/apps/auctions/selectors.py
+++
@@ -1,7 +0,0 @@
-from django.db.models import QuerySet
-
-from apps.auctions.models import Auction, AuctionStatus
-
-
-def live_auctions() -> QuerySet[Auction]:
-    return Auction.objects.filter(status=AuctionStatus.LIVE)
