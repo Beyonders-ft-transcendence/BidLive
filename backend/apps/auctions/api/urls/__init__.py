e8c16a7ecbdaa6a48047f3974979930ac2b9751a
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2026-05-21 15:42:23 +0100

    refactor: Mover a lógica do roteador para um arquivo separado para melhor organização.

--- backend/apps/auctions/api/urls/__init__.py
+++ backend/apps/auctions/api/urls/__init__.py
@@ -1,10 +1,6 @@
-from rest_framework.routers import DefaultRouter
+from apps.auctions.api.urls.router import build_router
 
-from apps.auctions.api.views.auction_views import AuctionViewSet
-from apps.auctions.api.views.category_views import AuctionCategoryViewSet
-
-router = DefaultRouter()
-router.register("auctions", AuctionViewSet, basename="auctions")
-router.register("categories", AuctionCategoryViewSet, basename="auction-categories")
-
+router = build_router()
 urlpatterns = router.urls
+
+__all__ = ["router", "urlpatterns", "build_router"]
