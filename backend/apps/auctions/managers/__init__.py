aca66077820bac12805e1dbe5e29d25c7a23ec9c
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2026-05-21 15:48:09 +0100

    feat: mover a lógica do gerenciador e do queryset de leilão para arquivos separados para melhor organização.

--- backend/apps/auctions/managers/__init__.py
+++ backend/apps/auctions/managers/__init__.py
@@ -1,25 +1,4 @@
-from django.db import models
+from apps.auctions.managers.auction_manager import AuctionManager
+from apps.auctions.managers.querysets import AuctionQuerySet
 
-class AuctionQuerySet(models.QuerySet):
-    def live(self):
-        return self.filter(status="LIVE")
-
-    def scheduled(self):
-        return self.filter(status="SCHEDULED")
-
-    def active(self):
-        return self.filter(status__in=["SCHEDULED", "LIVE"])
-
-
-class AuctionManager(models.Manager):
-    def get_queryset(self):
-        return AuctionQuerySet(self.model, using=self._db)
-
-    def live(self):
-        return self.get_queryset().live()
-
-    def scheduled(self):
-        return self.get_queryset().scheduled()
-
-    def active(self):
-        return self.get_queryset().active()
+__all__ = ["AuctionManager", "AuctionQuerySet"]
