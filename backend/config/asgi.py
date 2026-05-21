7f7d01a700a27077a5b3783c6281a05d48aef747
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2026-05-21 14:45:28 +0100

    refactor: reorganize os arquivos asgi.py e development.py para maior clareza e estrutura.

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,12 +1,14 @@
 import os
 
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
+django.setup()
+
 from apps.auctions.websocket.middleware import JWTAuthMiddleware
 from apps.auctions.websocket.routing import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
 
 application = ProtocolTypeRouter(
     {
