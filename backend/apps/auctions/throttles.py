2164d847ae35cd8680e4eb51a128922ffc1ff832
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2026-05-26 22:14:26 +0100

    feat: add bid anti-spam protections

---
+++ backend/apps/auctions/throttles.py
@@ -0,0 +1,17 @@
+from rest_framework.throttling import SimpleRateThrottle
+
+
+class BidUserThrottle(SimpleRateThrottle):
+    scope = "bid_user"
+
+    def get_cache_key(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            return None
+        return self.cache_format % {"scope": self.scope, "ident": request.user.pk}
+
+
+class BidIPThrottle(SimpleRateThrottle):
+    scope = "bid_ip"
+
+    def get_cache_key(self, request, view):
+        return self.cache_format % {"scope": self.scope, "ident": self.get_ident(request)}
