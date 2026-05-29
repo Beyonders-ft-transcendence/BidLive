e69aeb0716bbf676fd02adffe5c28be6292f6d29
Nd Daniel <108186371+NdondaDaniel2020@users.noreply.github.com>
2026-05-30 00:30:48 +0100

    feat(auctions): adicionar selectors de streams

--- backend/apps/auctions/selectors/__init__.py
+++ backend/apps/auctions/selectors/__init__.py
@@ -7,6 +7,14 @@ from apps.auctions.selectors.auction_selectors import (
     list_bids_for_auction,
     live_auctions,
 )
+from apps.auctions.selectors.stream_selectors import (
+    get_active_stream_for_auction,
+    get_stream_for_auction,
+    get_stream_viewer_count,
+    list_active_streams_for_auction,
+    list_streams_for_auction,
+    list_viewers_for_stream,
+)
 from apps.auctions.selectors.category_selectors import list_categories
 
 __all__ = [
@@ -18,4 +26,10 @@ __all__ = [
     "get_highest_bid_for_auction",
     "list_bids_for_auction",
     "live_auctions",
+    "list_streams_for_auction",
+    "get_stream_for_auction",
+    "get_active_stream_for_auction",
+    "list_viewers_for_stream",
+    "get_stream_viewer_count",
+    "list_active_streams_for_auction",
 ]
