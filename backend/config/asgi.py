e1c6f9d3cf87210f55b1c28aa4866030b3bcadb7
asebastiao <antoniosebasacs@gmail.com>
2026-06-01 20:28:23 +0100

    feat(websocket): integrate auction and chat websocket routing into ASGI application

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,5 +1,4 @@
 import os
-
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
@@ -8,11 +7,14 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
 django.setup()
 
 from apps.auctions.websocket.middleware import JWTAuthMiddleware
-from apps.auctions.websocket.routing import websocket_urlpatterns
+from apps.auctions.websocket.routing import websocket_urlpatterns as auction_ws
+from apps.chat.websocket.routing import websocket_urlpatterns as chat_ws
+
+all_websocket_patterns = auction_ws + chat_ws
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": JWTAuthMiddleware(URLRouter(websocket_urlpatterns)),
+        "websocket": JWTAuthMiddleware(URLRouter(all_websocket_patterns)),
     }
 )
