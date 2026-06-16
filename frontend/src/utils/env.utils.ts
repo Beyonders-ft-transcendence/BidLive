fbe3ac2f58fad88d87c938f24ad096fd699f204b
emanuel-malungo <emanuelmalungo856@gmail.com>
2026-06-16 12:28:46 +0100

    feat: add API_BASE_URL, WS_BASE_URL, and LIVEKIT_URL to environment configuration

--- frontend/src/utils/env.utils.ts
+++ frontend/src/utils/env.utils.ts
@@ -2,6 +2,9 @@
 
 const ENV = {
     API_URL: process.env.NEXT_PUBLIC_API_URL || '',
+    API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || '',
+    WS_BASE_URL: process.env.NEXT_PUBLIC_WS_BASE_URL || '',
+    LIVEKIT_URL: process.env.NEXT_PUBLIC_LIVEKIT_URL || '',
 }
 
 export default ENV
