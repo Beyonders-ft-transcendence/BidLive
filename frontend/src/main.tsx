ab8b1701b17f9ca9cfb2e37f95ff492374633fde
emanuel-malungo <emanuelmalungo856@gmail.com>
2026-06-17 18:48:58 +0100

    feat: implement theme management system with Tailwind CSS variables and initialization logic

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,12 @@
-import App from './App.tsx'
+import App from '@/App.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+
+import { initTheme } from '@/utils/themes.utils'
+
+// Initialize the theme before React renders to prevent FOUC
+initTheme()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
