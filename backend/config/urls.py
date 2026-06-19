1055ad4b5d1a2e965cd0e56e26b3d7289bfec199
Nd Daniel <108186371+NdondaDaniel2020@users.noreply.github.com>
2026-06-19 19:58:40 +0100

    feat: Configure os padrões de URL raiz do projeto, incluindo os endpoints de administração, API, documentação e métricas.

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,15 +4,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django_prometheus import exports
 
 urlpatterns = [
-    path("metrics/", include("django_prometheus.urls")),
     path("", lambda request: JsonResponse({"msg": "welcome in bidlive"}), name="api-root"),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    path("", include("django_prometheus.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("metrics/", exports.ExportToDjangoView, name="prometheus-django-metrics-slash"),
 ]
 
 if settings.DEBUG:
