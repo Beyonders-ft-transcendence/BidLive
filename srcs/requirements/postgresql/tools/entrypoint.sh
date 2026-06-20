154439f0825bd1e2b8e5e255a05ccfff22194d4c
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2026-06-20 18:48:45 +0100

    refactor: restruturação da forma que as credenciais são tratadas no postgresql

--- srcs/requirements/postgresql/tools/entrypoint.sh
+++ srcs/requirements/postgresql/tools/entrypoint.sh
@@ -1,21 +1,36 @@
 #!/bin/sh
 set -e
 
+if [ -f /run/secrets/db_credenciais ]; then
+    POSTGRES_USER=$(sed -n '1p' /run/secrets/db_credenciais | cut -d'=' -f2 | tr -d '\r')
+    POSTGRES_PASSWORD=$(sed -n '2p' /run/secrets/db_credenciais | cut -d'=' -f2 | tr -d '\r')
+    POSTGRES_DB=$(sed -n '3p' /run/secrets/db_credenciais | cut -d'=' -f2 | tr -d '\r')
+    POSTGRES_PORT=$(sed -n '4p' /run/secrets/db_credenciais | cut -d'=' -f2 | tr -d '\r')
+else
+    echo "Error: No se encontró el archivo de credenciales."
+    exit 1
+fi    
+
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
-    export POSTGRES_PASSWORD="$(cat /run/secrets/db_password)"
+    
+    mkdir -p "$PGDATA"
+    chown -R postgres:postgres "$PGDATA"
+    chmod 700 "$PGDATA"
 
-    initdb -D "$PGDATA"
+    echo "Inicializando o banco de dados..."
+    su-exec postgres initdb -D "$PGDATA" -o "-c listen_addresses='*' -c port='$POSTGRES_PORT'"
 
-    pg_ctl -D "$PGDATA" -o "-c listen_addresses='*'" -w start
+    echo "Iniciando temporariamente para configuração..."
+    su-exec postgres pg_ctl -D "$PGDATA" -o "-c listen_addresses='*' -c port='$POSTGRES_PORT'" -w start
 
-    psql -U postgres -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
+    echo "Criando usuários e bancos..."
+    su-exec postgres psql -p "$POSTGRES_PORT" -U postgres -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
+    su-exec postgres psql -p "$POSTGRES_PORT" -U postgres -c "CREATE DATABASE $POSTGRES_DB OWNER $POSTGRES_USER;"
+    su-exec postgres psql -p "$POSTGRES_PORT" -U postgres -c "ALTER USER $POSTGRES_USER CREATEDB;"
 
-    psql -U postgres -c "CREATE DATABASE $POSTGRES_DB OWNER $POSTGRES_USER;"
-
-    psql -U postgres -c "ALTER USER $POSTGRES_USER CREATEDB;"
-
-    pg_ctl -D "$PGDATA" -m fast -w stop
+    echo "Desligando a instância temporária..."
+    su-exec postgres pg_ctl -D "$PGDATA" -m fast -w stop
 fi
 
-echo "Starting PostgreSQL..."
-exec postgres -D "$PGDATA" -c listen_addresses='*'
+echo "Starting PostgreSQL na porta $POSTGRES_PORT..."
+exec su-exec postgres postgres -D "$PGDATA" -c listen_addresses='*' -c port="$POSTGRES_PORT"
