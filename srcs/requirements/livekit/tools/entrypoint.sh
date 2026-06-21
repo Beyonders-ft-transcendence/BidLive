7be058f2a4c4b28f8e2ea9c17850b759670c965d
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2026-06-21 14:45:48 +0100

    fix(Bug): O erro do livekit resolvido parsilmente

--- srcs/requirements/livekit/tools/entrypoint.sh
+++ srcs/requirements/livekit/tools/entrypoint.sh
@@ -15,15 +15,12 @@ if [ -f /run/secrets/livekit_credenciais ] && [ -f /run/secrets/redis_credenciai
     LIVEKIT_RTC_UDP_PORT_RANGE_START=$(sed -n '7p' /run/secrets/livekit_credenciais | cut -d'=' -f2 | tr -d '\r')
     LIVEKIT_RTC_UDP_PORT_RANGE_END=$(sed -n '8p' /run/secrets/livekit_credenciais | cut -d'=' -f2 | tr -d '\r')
 
-    # CORRIGIDO: O LiveKit não usa o formato de URI padrão do Redis aqui.
-    # Guardamos apenas o endereço "host:porta" e a senha separada.
     REDIS_ADDRESS="redis:${REDIS_PORT}"
 else
   echo "No credentials found"
   exit 1
 fi  
 
-# Mudado para /tmp para garantir permissão de escrita total do usuário livekit
 CONFIG_FILE="/tmp/livekit.yaml"
 
 echo "Generating LiveKit configuration..."
@@ -47,6 +44,7 @@ redis:
   address: ${REDIS_ADDRESS}
   username: ${REDIS_USER}
   password: ${REDIS_PASSWORD}
+  db: 0
   read_timeout: 5
   write_timeout: 5
 EOF
