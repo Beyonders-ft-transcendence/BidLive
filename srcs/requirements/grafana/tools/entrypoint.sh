1b31d452a79baf6324537226bd4e5ccef13f00c4
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2026-06-21 21:22:49 +0100

    feat: add imagens Docker do Grafana e do frontend com arquivos de compilação e configuração.

--- srcs/requirements/grafana/tools/entrypoint.sh
+++ srcs/requirements/grafana/tools/entrypoint.sh
@@ -3,14 +3,12 @@ set -e
 
 echo "Starting Grafana..."
 
-# usa secrets ou env var
 if [ -f /run/secrets/grafana_password ]; then
   export GF_SECURITY_ADMIN_PASSWORD=$(cat /run/secrets/grafana_password)
 fi
 
-exec grafana-server \
-  --homepath=/usr/share/grafana \
-  --config=/etc/grafana/grafana.ini \
-  cfg:default.paths.data=/var/lib/grafana \
-  cfg:default.paths.logs=/var/log/grafana \
-  cfg:default.server.http_port=3000
+export GF_PATHS_DATA="${GF_PATHS_DATA:-/var/lib/grafana}"
+export GF_PATHS_LOGS="${GF_PATHS_LOGS:-/var/log/grafana}"
+export GF_SERVER_HTTP_PORT="${GF_SERVER_HTTP_PORT:-3000}"
+
+exec /run.sh
