184f4ff027f1d20e5d33150f09dc2a9f7af61c84
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2026-06-21 16:04:51 +0100

    feat: Implementar arquitetura multisserviços do Docker com configuração de ambiente baseada em segredos.

--- srcs/requirements/redis/tools/entrypoint.sh
+++ srcs/requirements/redis/tools/entrypoint.sh
@@ -2,13 +2,9 @@
 set -e
 
 if [ -f /run/secrets/redis_credenciais ]; then
-    REDIS_USER=$(sed -n '1p' /run/secrets/redis_credenciais | cut -d'=' -f2 | tr -d '\r')
-    REDIS_PASSWORD=$(sed -n '2p' /run/secrets/redis_credenciais | cut -d'=' -f2 | tr -d '\r')
-    REDIS_PORT=$(sed -n '3p' /run/secrets/redis_credenciais | cut -d'=' -f2 | tr -d '\r')
+    REDIS_PASSWORD=$(sed -n '1p' /run/secrets/redis_credenciais | cut -d'=' -f2 | tr -d '\r')
 
-    sed -i "s/REDIS_USER_PLACEHOLDER/$REDIS_USER/g" /etc/redis/redis.conf
     sed -i "s/REDIS_PASSWORD_PLACEHOLDER/$REDIS_PASSWORD/g" /etc/redis/redis.conf
-    sed -i "s/REDIS_PORT_PLACEHOLDER/$REDIS_PORT/g" /etc/redis/redis.conf
 fi  
 
 exec redis-server /etc/redis/redis.conf
