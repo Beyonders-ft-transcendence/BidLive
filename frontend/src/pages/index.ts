3755fedda5500dfa942647fe63a3fdffb054c177
emanuel-malungo <emanuelmalungo856@gmail.com>
2026-06-29 11:27:33 +0100

    feat: implement auction detail page and register new application routes

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,6 +2,7 @@
 export { default as SigninPage } from "./auth/signin";
 export { default as HomePage } from "./Home";
 export { default as AuctionsPage } from "./Auctions";
+export { default as AuctionDetailPage } from "./AuctionDetail";
 export { NotFoundPage } from "./errors/NotFoundPage";
 export { UserDashboard } from "./dashboard/UserDashboard";
 export { BackofficeDashboard } from "./dashboard/BackofficeDashboard";
