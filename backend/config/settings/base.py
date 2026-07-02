187eb221f79348151ef20772a3d2a61c9bb1e0d8
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2026-07-02 10:44:50 +0100

    feat: configuração de conteinerização de backend

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -346,5 +346,3 @@ EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
 EMAIL_HOST_USER = env("EMAIL_HOST_USER", default=env("EMAIL_USER", default=""))
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default=env("EMAIL_PASSWORD", default=""))
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default=EMAIL_HOST_USER)
-
-
